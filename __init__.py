f6ff6e92a11f8320887a9b897952a1de5d09c0ef
fernando-inwk <Fernando.Silva@inwk.com>
2026-05-14 00:20:24 -0300

    some fixes

--- __init__.py
+++ __init__.py
@@ -1,32 +1,18 @@
 # MIT License
-#
 # Copyright (c) 2023 Thomas 'noio' van den Berg
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
-# NOTE: bl_info has been removed. Blender 5+ uses blender_manifest.toml instead.
-# See: https://docs.blender.org/manual/en/latest/advanced/extensions/addons.html
+# Blender 5 port by community — auto_load.py fix for extension package naming
 
 import bpy
-from . import auto_load
+import traceback
 
-auto_load.init()
+try:
+    from . import auto_load
+    print(f"[PixelUnwrap] __package__ = {__package__}")
+    auto_load.init(__package__)
+    print(f"[PixelUnwrap] auto_load.init() succeeded")
+except Exception as e:
+    print(f"[PixelUnwrap] ERROR during init: {e}")
+    traceback.print_exc()
 
 
 def register():
--- auto_load.py
+++ auto_load.py
@@ -17,11 +17,11 @@ blender_version = bpy.app.version
 modules = None
 ordered_classes = None
 
-def init():
+def init(package_name):
     global modules
     global ordered_classes
 
-    modules = get_all_submodules(Path(__file__).parent)
+    modules = get_all_submodules(Path(__file__).parent, package_name)
     ordered_classes = get_ordered_classes_to_register(modules)
 
 def register():
@@ -29,7 +29,7 @@ def register():
         bpy.utils.register_class(cls)
 
     for module in modules:
-        if module.__name__ == __name__:
+        if module.__spec__.origin == __spec__.origin:
             continue
         if hasattr(module, "register"):
             module.register()
@@ -39,7 +39,7 @@ def unregister():
         bpy.utils.unregister_class(cls)
 
     for module in modules:
-        if module.__name__ == __name__:
+        if module.__spec__.origin == __spec__.origin:
             continue
         if hasattr(module, "unregister"):
             module.unregister()
@@ -48,8 +48,8 @@ def unregister():
 # Import modules
 #################################################
 
-def get_all_submodules(directory):
-    return list(iter_submodules(directory, directory.name))
+def get_all_submodules(directory, package_name):
+    return list(iter_submodules(directory, package_name))
 
 def iter_submodules(path, package_name):
     for name in sorted(iter_submodule_names(path)):
